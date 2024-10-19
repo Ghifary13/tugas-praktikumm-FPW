ecb7912c99676fa797aeaf4a87da58f085e11b8d
Ghifary13 <2210631170016@student.unsika.ac.id>
2024-10-19 21:09:29 +0700

    tugas pratikum supplier

--- pertemuan_4_dan_5/routes/web.php
+++ pertemuan_4_dan_5/routes/web.php
@@ -3,16 +3,7 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\productcontroller;
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+use App\Http\Controllers\SupplierController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -20,15 +11,6 @@ Route::get('/', function () {
 
 Route::get('/products/create', [ProductController::class, 'create'])->name('products-create');
 Route::post('/products', [ProductController::class, 'store'])->name('products-store');
-
-
-Route::get('/product',[ProductController::class, 'index']);
-Route::get('/product/create',[ProductController::class, 'create']);
-Route::post('/product',[ProductController::class, 'store']);
-Route::get('/product/{id}',[ProductController::class, 'show']);
-Route::get('/product/{id}/edit',[ProductController::class, 'edit']);
-Route::put('/product/{id}',[ProductController::class, 'update']);
-Route::delete('/product/{id}',[ProductController::class, 'destroy']);
 
 
 
@@ -42,7 +24,24 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified', 'RoleCheck:admin'])->name('dashboard');
+
+
+Route::get('/product', [ProductController::class, 'index']);
+Route::get('/product/create', [ProductController::class, 'create']);
+Route::post('/product', [ProductController::class, 'store']);
+Route::get('/product/{id}', [ProductController::class, 'show']);
+Route::get('/product/{id}/edit', [ProductController::class, 'edit']);
+Route::put('/product/{id}', [ProductController::class, 'update']);
+Route::delete('/product/{id}', [ProductController::class, 'destroy']);
+
+Route::get('/supplier', [SupplierController::class, 'index']);
+Route::get('/supplier/create', [SupplierController::class, 'create'])->name("supplier-create");
+Route::post('/supplier', [SupplierController::class, 'store'])->name("supplier-store");
+Route::get('/supplier/{id}', [SupplierController::class, 'show']);
+Route::get('/supplier/{id}/edit', [SupplierController::class, 'edit']);
+Route::put('/supplier/{id}', [SupplierController::class, 'update']);
+Route::delete('/supplier/{id}', [SupplierController::class, 'destory']);
