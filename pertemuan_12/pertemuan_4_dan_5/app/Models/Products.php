3f0114bf427b36b158dc48216c30976825afca66
Ghifary13 <2210631170016@student.unsika.ac.id>
2024-11-26 13:43:34 +0700

    tugas bab 12

---
+++ pertemuan_12/pertemuan_4_dan_5/app/Models/Products.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Products extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'product_name',
+        'unit',
+        'type',
+        'information',
+        'qty',
+        'producer',
+        'supplier_id'
+    ];
+
+    public function supplier()
+    {
+        return $this->belongsTo(supplier::class);
+    }
+}
